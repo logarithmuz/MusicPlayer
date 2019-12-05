1771511ab6b4eeffad06bac01af07cf02e260416
polarode <40320189+polarode@users.noreply.github.com>
2019-12-05 23:47:49 +0100

    Update look of listitems

--- app/src/main/java/de/elite/musicplayer/MyItemRecyclerViewAdapter.java
+++ app/src/main/java/de/elite/musicplayer/MyItemRecyclerViewAdapter.java
@@ -65,8 +65,8 @@ public class MyItemRecyclerViewAdapter extends RecyclerView.Adapter<MyItemRecycl
         public ViewHolder(View view) {
             super(view);
             mView = view;
-            mIdView = (TextView) view.findViewById(R.id.item_number);
-            mContentView = (TextView) view.findViewById(R.id.content);
+            mIdView = (TextView) view.findViewById(R.id.song_title);
+            mContentView = (TextView) view.findViewById(R.id.song_artist);
         }
 
         @Override
