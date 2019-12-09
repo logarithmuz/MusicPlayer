23f2b7304ec466cd79636b41fc160721dc418296
polarode <40320189+polarode@users.noreply.github.com>
2019-12-09 22:38:52 +0100

    MusicPlayer can now play music
    epic

--- app/src/main/java/de/elite/musicplayer/ui/main/MusicPlayer.java
+++ app/src/main/java/de/elite/musicplayer/ui/main/MusicPlayer.java
@@ -1,10 +1,19 @@
 package de.elite.musicplayer.ui.main;
 
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+
+import java.io.IOException;
+
+import de.elite.musicplayer.Song;
+
 public class MusicPlayer {
 
     private static MusicPlayer instance;
 
     private PlayerState playerState = PlayerState.PAUSE;
+    private MediaPlayer mediaPlayer = new MediaPlayer();
 
     private MusicPlayer() {
     }
@@ -18,9 +27,22 @@ public class MusicPlayer {
 
     public void playPause() {
         if (this.playerState == PlayerState.PAUSE) {
+            mediaPlayer.start();
             this.playerState = PlayerState.PLAY;
         } else if (this.playerState == PlayerState.PLAY) {
+            mediaPlayer.pause();
             this.playerState = PlayerState.PAUSE;
+        }
+    }
+
+    public void playSong(Context context, Song song) {
+        try {
+            mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+            mediaPlayer.setDataSource(context, song.getUri());
+            mediaPlayer.prepare();
+            mediaPlayer.start();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
